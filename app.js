8116b6e3b4b20380a2b0ffd4544e184265b7f237
DevWesly <dev.lucaswesley@gmail.com>
2022-02-18 10:52:47 -0300

    Adicionando Países

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 let paises = []
+let lista = ['Brasil', 'Argentina', 'Colombia','Peru','Chile', 'Equador', 'Bosnia', 'Venezuela', 'Bolívia', 'Uruguai', 'Guiana', 'Suriname', 'Paraguai', 'Guiana Francesa']
+paises.push(lista)
+console.log(paises)
